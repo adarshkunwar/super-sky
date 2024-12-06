6b72c7ca9252dd4b67286daff30a60accffa79f2
adarshkunwar <adarshkunwarofficial@gmail.com>
2024-12-06 20:23:09 +0545

    feat: add notification type

---
+++ src/types/notification.d.ts
@@ -0,0 +1,9 @@
+type TNotificationType = "like" | "comment" | "follow" | "repost";
+
+export type TSingleNotifcation = {
+  type: TNotificationType;
+  title: string;
+  message: string;
+  timestamp: string;
+  onClose: () => void;
+};
